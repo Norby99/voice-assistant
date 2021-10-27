a6946326a0ae3015d54392bd137a1d226f3542e6
Norby99 <dont.vip@gmail.com>
2021-10-28 01:29:24 +0200

    Added the conditioner and the arduino classes

---
+++ libraries/audio.py
@@ -0,0 +1,66 @@
+import speech_recognition as sr
+
+class Vocal():
+
+    assistantName = "patric"  # the assistant name. Change it as u like
+    language = "it-IT"
+    # this are the commands that can be executed by the bot
+    coldAir = ["ho caldo", "fa caldo", "aria fredda"]
+    hotAir = ["ho freddo", "fa freddo", "aria calda"]
+    conditionerPowerOff = ["spegni il condizionatore", "spegni l'aria condizionata", "stacca il condizionatore", "stacca l'aria condizionata"]
+
+
+    def __init__(self):
+        self.r = sr.Recognizer()
+
+    def listen(self):   # short example function
+        with sr.Microphone() as source:
+            self.r.adjust_for_ambient_noise(source)
+            print("In ascolto...")
+            audio = self.r.listen(source)
+            try:
+                text = self.r.recognize_google(audio, language="it-IT")
+                if text.lower() == "ciao" or (("raul" in text.lower() and "ciao" in text.lower())):     #saluto
+                    return "Ciao"
+                else:                                                                                  #non saluto
+                    if "raul".lower() not in text.lower():
+                        
+                        return text
+                    else:
+                        return "Si signore!", text
+            except:
+                return "Non ho capito!"
+
+    def wakeUp(self):   # checks if raul is called
+        with sr.Microphone() as source:
+            self.r.adjust_for_ambient_noise(source)
+            audio = self.r.listen(source)
+            try:
+                text = self.r.recognize_google(audio, language=self.language)
+                if self.assistantName in text.lower():     #saluto
+                    return True
+            except:
+                pass
+        return False
+
+    def Commands(self):
+        with sr.Microphone() as source:
+            self.r.adjust_for_ambient_noise(source)
+            audio = self.r.listen(source)
+            try:
+                text = self.r.recognize_google(audio, language=self.language)
+                if any(x in text.lower() for x in self.coldAir):
+                    return ["conditioner", "cold"]
+                elif any(x in text.lower() for x in self.hotAir):
+                    return ["conditioner", "hot"]
+                elif any(x in text.lower() for x in self.conditionerPowerOff):
+                    return ["conditioner", "off"]
+                else:
+                    print("No command identified: " + text.lower())
+            except:
+                return False
+
+
+if __name__ == "__main__":
+    audio = Vocal()
+    print(audio.listen())
---
+++ libraries/conditioner.py
@@ -0,0 +1,33 @@
+import time
+import threading
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) # I must do in this way to use it both as a library and a separate program
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from libraries.arduino_interface import *
+
+class Conditioner():
+
+    def __init__(self):
+        self.arduino = Arduino()
+        self.powerStatus = 0
+        time.sleep(2)
+
+    def setStatus(self, command, repeat=True):   # takes a comand and executes it
+        self.arduino.WriteSignal(command)
+        if repeat:
+            threading.Thread(target = self.repeatCommand, args=(command,)).start()
+
+    def debug(self):    # it's used just to print the signal input
+        print(self.arduino.ReadSignal())
+
+    def repeatCommand(self, command):   # this function repeats the last istruction twice, because sometimes the conditioner doesn't execute it at first
+        time.sleep(2)
+        self.setStatus(command, False)
+
+if __name__ == "__main__":
+
+    cond = Conditioner()
+    cond.setStatus("off")
